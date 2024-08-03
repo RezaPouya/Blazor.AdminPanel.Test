15972c7a7bb2e4fdd5dc409dd349cd52f8149a25
r.pouya <rpouya@hotmail.com>
2024-08-03 13:58:25 +0330

    initialized

--- src/RefahBank.Beta.Branch.Panel.EndPoint/wwwroot/scripts/comps/random-number.js
+++
@@ -1,9 +0,0 @@
-
-export function displayAlertMessage(count) {
-    console.log('the count is ' + count);
-    alert('the count is ' + count);
-}
-
-//export function createMessage(count) {
-//    return 'the count is ' + count;
-//}
